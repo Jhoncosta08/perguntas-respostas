e929f913d64e4271d7f18952e182834dea550993
Jhonatan Silva da Costa <jhonatan.costa@tuntscorp.com>
2022-10-23 17:28:29 -0300

    feat(project): Capturando dados de um formulário

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
@@ -15,7 +17,9 @@ app.get('/perguntar', (req, res) => {
 });
 
 app.post('/salvarpergunta', (req, res) => {
-    res.send('Formulário recebido');
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;
+    res.send(`Formulário recebido, Titulo: ${titulo}, descrição: ${descricao}`);
 });
 
 
