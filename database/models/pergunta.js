64118790dc2452df90dc5c28dc5ff2c1666ff82c
Jhonatan Silva da Costa <jhonatan.costa@tuntscorp.com>
2022-10-23 18:13:26 -0300

    feat(project): Gerando tabela com model

---
+++ database/models/pergunta.js
@@ -0,0 +1,19 @@
+const sequelize = require('sequelize');
+const connection = require('../database');
+
+const pergunta = connection.define('pergunta', {
+    titulo: {
+        type: sequelize.STRING,
+        allowNull: false
+    },
+    descricao: {
+        type: sequelize.TEXT,
+        allowNull: false
+    }
+});
+
+pergunta.sync({force: false}).then(() => {
+    console.log('Tabela pergunta criada com sucesso.');
+}).catch((error => {
+    console.log(error);
+}));
